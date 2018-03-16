9f4ec755995fe5f4dc96b6a0574e4b510d206446
Marek Tlacbaba <marek.tlacbaba@gmail.com>
2018-03-16 16:54:42 +0800

    added skeleton for aws lambda projects for uploading data into soar

---
+++ Soar/Soar.Core/Responses/UploadLink.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Soar.Core.Responses
+{
+    public class UploadLink
+    {
+        public string Challenge { get; set; }
+        public string Secret { get; set; }
+    }
+}
