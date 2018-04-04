c7bcd8689f0752711373b50f180244ebb99d04b5
Chris Lowe <chris@takor.com.au>
2018-04-04 16:10:31 +0800

    Browse item details

--- react-app/src/components/Browse/BrowseItem.js
+++ react-app/src/components/Browse/BrowseItem.js
@@ -3,6 +3,7 @@ import parse from 'wellknown';
 import Geocode from 'react-geocode';
 import Async from 'react-promise';
 import { Grid, Row, Col } from 'react-bootstrap';
+import BrowseDetailRow from './BrowseDetailRow';
 import './BrowseItem.css'
 
 const browseItem = (props) => {
@@ -18,39 +19,30 @@ const browseItem = (props) => {
     let p = parse(upload.pointWKT);
     let position = [p.coordinates[1], p.coordinates[0]];
     let info = JSON.parse(upload.metadata);
-    let droneCamera = info.make + " / " + info.model;
+    let droneMake = info.make;
+    let droneModel = info.model;
     let date = new Date(info.date);
-    // let geocode = Geocode.fromLatLng(position[0], position[1]).then(
-    //     response => {
-    //         const address = response.results[0].formatted_address;
-    //         console.log(address);
-    //       },
-    //       error => {
-    //         console.error(error);
-    //       }
-    //     );
 
-    let getGeocode = function() {
-        return Geocode.fromLatLng(position[0], position[1]);
-    }
-
-    let prom = new Promise(function(resolve, reject) {
+    let geocodePromise = new Promise(function(resolve, reject) {
         setTimeout(function() {
           resolve(Geocode.fromLatLng(position[0], position[1]));
         }, 100)
       })
+
+    let geocodeAsync = <Async promise={geocodePromise} then={val => <p>{val.results[0].formatted_address}</p>} />
+        
     
-
-
+    
     return (
         <Col md={4} sm={12} className="BrowseItem">
-            <img src={upload.previewUrl} alt={upload.previewUrl} />
-                <div><label>Address: </label><Async promise={prom} then={val => <p>{val.results[0].formatted_address}</p>} /></div>
-                <div><label>Drone/Camera: </label>{droneCamera}</div>
-                <div><label>Resolution: </label>{info.dimX + " x " + info.dimY}</div>
-                <div><label>Date: </label>{date.toDateString()}</div>
-                <div><label>Latitude: </label>{position[1]}</div>
-                <div><label>Longitude: </label>{position[0]}</div>
+            <Row>
+                <img src={upload.previewUrl} alt={upload.previewUrl} />
+            </Row>
+            <BrowseDetailRow label="Address" value={geocodeAsync} />
+            <BrowseDetailRow label="Drone" value={droneMake} />
+            <BrowseDetailRow label="Camera" value={droneModel} />
+            <BrowseDetailRow label="Resolution" value={info.dimX + "x" + info.dimY} />
+            <BrowseDetailRow label="Date" value={date.toDateString} />
         </Col>
     )
 }
