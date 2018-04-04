c2b19daa6e06bb9f3557e4eb41ee984f03d5c484
Marek Tlacbaba <marek.tlacbaba@gmail.com>
2018-04-04 15:40:45 +0800

    updated migration script to set Pricing during the deployment process

--- smart-contracts/migrations/2_deploy_contracts.js
+++ smart-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,13 @@
 var Soar = artifacts.require("./Soar.sol");
 var PricingManual = artifacts.require("./PricingManual.sol");
 
-
 module.exports = function(deployer) {
-  deployer.deploy(PricingManual);
-  deployer.deploy(Soar);
+  let promises = [];
+  promises.push(deployer.deploy(PricingManual));
+  promises.push(deployer.deploy(Soar));
+  Promise.all(promises).then(results => {
+    return Soar.deployed();
+  }).then(instance => {
+    return instance.setPricingContract(PricingManual.address);
+  });
 };
