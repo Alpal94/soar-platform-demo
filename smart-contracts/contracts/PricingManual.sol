612b917225279ba4721e1e09365ce2a9d2965ccb
Marek Tlacbaba <marek.tlacbaba@gmail.com>
2018-04-27 14:02:13 +0800

    updated manual pricing to allow to set price for 2-7 precision geohash levels

--- smart-contracts/contracts/PricingManual.sol
+++ smart-contracts/contracts/PricingManual.sol
@@ -12,7 +12,12 @@ contract PricingManual is Ownable, IPricing {
 
     uint256 private defaultPrice = 1 * (uint(10) ** 18);
 
-    mapping (bytes5 => uint256) internal prices;
+    mapping (bytes2 => uint256) internal prices2;
+    mapping (bytes3 => uint256) internal prices3;
+    mapping (bytes4 => uint256) internal prices4;
+    mapping (bytes5 => uint256) internal prices5;
+    mapping (bytes6 => uint256) internal prices6;
+    mapping (bytes7 => uint256) internal prices7;
 
     function PricingManual() public {
     }
@@ -20,19 +25,52 @@ contract PricingManual is Ownable, IPricing {
     function getPrice(bytes12 _geoHash) whenNotPaused public view returns (
         uint256 price_
     ) {
-        price_ = prices[bytes5(_geoHash)];
+        if(prices2[bytes2(_geoHash)] > 0){
+            price_ = prices2[bytes2(_geoHash)];
+        }
+        if(prices3[bytes3(_geoHash)] > 0){
+            price_ = prices3[bytes3(_geoHash)];
+        }
+        if(prices4[bytes4(_geoHash)] > 0){
+            price_ = prices4[bytes4(_geoHash)];
+        }
+        if(prices5[bytes5(_geoHash)] > 0){
+            price_ = prices5[bytes5(_geoHash)];
+        }
+        if(prices6[bytes6(_geoHash)] > 0){
+            price_ = prices6[bytes6(_geoHash)];
+        }
+        if(prices7[bytes7(_geoHash)] > 0){
+            price_ = prices7[bytes7(_geoHash)];
+        }
         if(price_ == 0){
             price_ = defaultPrice;
         }
     }
 
-    function setPrice(bytes5 _geoHash, uint256 _price) onlyOwner public {
-        prices[_geoHash] = _price;
+    function setPrice(bytes12 _geoHash, uint256 _price, uint256 _precision) onlyOwner public {
+        setPriceInternal(_geoHash, _price, _precision);
+    }
+
+    function setPriceInternal(bytes12 _geoHash, uint256 _price, uint256 _precision) onlyOwner internal {
+        if(_precision == 2){
+            prices2[bytes2(_geoHash)] = _price;
+        } else if (_precision == 3) {
+            prices3[bytes3(_geoHash)] = _price;
+        } else if (_precision == 4) {
+            prices4[bytes4(_geoHash)] = _price;
+        } else if (_precision == 5) {
+            prices5[bytes5(_geoHash)] = _price;
+        } else if (_precision == 6) {
+            prices6[bytes6(_geoHash)] = _price;
+        } else if (_precision == 7) {
+            prices7[bytes7(_geoHash)] = _price;
+        }
     }
     
-    function setPrices(bytes5[] _geoHashes, uint256 _price) onlyOwner public {
+    function setPrices(bytes12[] _geoHashes, uint256 _price, uint256 _precision) onlyOwner public {
         for (uint256 i = 0; i < _geoHashes.length; i++) {
-            prices[_geoHashes[i]] = _price;
+            setPriceInternal(_geoHashes[i], _price, _precision);
         }
     }
 }
