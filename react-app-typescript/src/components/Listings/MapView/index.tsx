582678b2a5d01dc8a9dcbb623770092a699b1638
Chris Lowe <chris@takor.com.au>
2018-04-27 16:33:51 +0800

    Adds mapbox satellite and street to listings

--- react-app-typescript/src/components/Listings/MapView/index.tsx
+++ react-app-typescript/src/components/Listings/MapView/index.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { TileLayer, Map } from 'react-leaflet';
+import { TileLayer, LayersControl, BaseLayer, Map, Popup, Marker } from 'react-leaflet';
 import './index.css';
 import { Listing, ListingsInfo, Sale } from '../../../lib/model';
 import ListingMarker from './listing-marker';
@@ -14,6 +14,8 @@ interface MapViewProps {
     download: (listing: Listing) => void;
 }
 
+const { BaseLayer, Overlay } = LayersControl;
+
 const MapView: React.SFC<MapViewProps> = (props) => {
 
     const position = [-31.9550404, 115.9303017];
@@ -21,22 +23,33 @@ const MapView: React.SFC<MapViewProps> = (props) => {
     return (
         <div className="listings-map-container">
             <Map center={position} zoom={zoom} className="wizard-map">
-                <TileLayer
-                    attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-                    url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-                />
-                {props.listings.map(listing =>
-                    <ListingMarker
-                        key={listing.filehash}
-                        listing={listing}
-                        info={props.info}
-                        price={props.prices.get(listing.geohash)}
-                        priceUpdate={props.priceUpdate}
-                        purchase={props.purchases.get(listing.filehash)}
-                        buy={props.buy}
-                        download={props.download}
-                    />
-                )}
+                <LayersControl position="topright">
+                    <BaseLayer name="Mapbox Satellite">
+                        <TileLayer
+                        attribution=""
+                        url="https://api.tiles.mapbox.com/v4/mapbox.satellite/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hyaXNsb3dlIiwiYSI6ImYzZmI2OWM3MDgyNWQ0MTdmZTRkYWM5MjVlN2JlY2Q3In0.3aXTSJOkgeC_obQ6TDmlcw"
+                        />
+                    </BaseLayer>
+                    <BaseLayer checked name="Mapbox Street">
+                        <TileLayer
+                        attribution=""
+                        url="https://api.tiles.mapbox.com/v4/digitalglobe.nako6329/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2hyaXNsb3dlIiwiYSI6ImYzZmI2OWM3MDgyNWQ0MTdmZTRkYWM5MjVlN2JlY2Q3In0.3aXTSJOkgeC_obQ6TDmlcw"
+                        />
+                    </BaseLayer>
+                    
+                    {props.listings.map(listing =>
+                        <ListingMarker
+                            key={listing.filehash}
+                            listing={listing}
+                            info={props.info}
+                            price={props.prices.get(listing.geohash)}
+                            priceUpdate={props.priceUpdate}
+                            purchase={props.purchases.get(listing.filehash)}
+                            buy={props.buy}
+                            download={props.download}
+                        />
+                    )}
+                </LayersControl>
             </Map>
             <div className="listings-map-info">Total: {props.info.listingsCount}</div>
         </div>
