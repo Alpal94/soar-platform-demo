5e0bb76c46c5c313ae99ba15d76ce09ee51487b2
Chris Lowe <chris@takor.com.au>
2018-05-02 10:54:06 +0800

    Removes old ES6 app

--- react-app/src/actions/progressActions.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../constants/actionTypes';
-
-export const progressText = () => ({
-  type: types.PROGESS_TEXT
-});
-
-export const progressComplete = () => ({
-  type: types.PROGRESS_COMPLETE
-});
--- react-app/src/helpers/ExifHelper.js
+++
@@ -1,7 +0,0 @@
-const toDecimal = function (number) {
-    return number[0].numerator + number[1].numerator /
-        (60 * number[1].denominator) + number[2].numerator / (3600 * number[2].denominator);
-};
-
-export default toDecimal;
-
